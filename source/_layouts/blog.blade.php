b53da8760ebd6b187a1ea7274e3ba5b0c7a77ee9
Franz Liedke <franz@develophp.org>
2016-04-28 23:50:25 +0900

    Use article element

--- source/_layouts/blog.blade.php
+++ source/_layouts/blog.blade.php
@@ -2,10 +2,10 @@
 
 @section('body')
 
-    <div class="article">
+    <article>
         <h1>{{ $title }}</h1>
         <time class="article-date" datetime="{{ $date }}T12:00">{{ $date }}</time>
         @yield('content')
-    </div>
+    </article>
 
 @endsection
