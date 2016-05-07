f06e52eebc82d77387f4a9994bb4a1a466cc3997
Franz Liedke <franz@develophp.org>
2016-05-07 12:22:31 +0900

    Show last three articles on homepage, too

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,24 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+
+    <h1>Hi, I'm Franz Liedke.</h1>
+    <h2>Software Developer, Open-Source Enthusiast.</h2>
+
+    <p>...</p>
+
+    <a href="#">Twitter</a>
+    <a href="#">GitHub</a>
+
+    <h2>Latest articles</h2>
+
+    @foreach (getArticles()->sortByDesc('date')->take(3) as $article)
+        <article class="link">
+            <time>{{ $article['date'] }}</time>
+            <h3>
+                <a href="/blog/{{ $article['filename'] }}.html">{{ $article['title'] }}</a>
+            </h3>
+        </article>
+    @endforeach
+
 @endsection
