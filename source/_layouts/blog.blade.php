2051c98cfb48da04062c384e1b2f96e4bfc1a34e
Franz Liedke <franz@develophp.org>
2017-02-05 20:22:06 +0100

    Include Disqus comments on every blog article

--- source/_layouts/blog.blade.php
+++ source/_layouts/blog.blade.php
@@ -6,6 +6,8 @@
         <h1>{{ $title }}</h1>
         <time class="article-date" datetime="{{ $date }}T12:00">{{ $date }}</time>
         @yield('content')
+
+        @include('_partials.disqus')
     </article>
 
 @endsection
