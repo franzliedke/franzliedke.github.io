f0d9270d529054bd07d99d9c836f0e860ff198a6
Franz Liedke <franz@develophp.org>
2023-05-26 23:58:58 +0200

    Update all the dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,26 +1,23 @@
-let mix = require('laravel-mix');
-let build = require('./tasks/build.js');
+const mix = require('laravel-mix');
+require('laravel-mix-jigsaw');
 
 let bourbon = require('bourbon');
 let neat = require('bourbon-neat');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
-mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync(),
-        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
-    ]
-});
 
-mix.js('source/_assets/js/main.js', 'js')
+mix.jigsaw()
+    .js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/sass/main.scss', 'css', {
-        includePaths: [].concat(
-          bourbon.includePaths,
-          neat.includePaths
-        )
+        sassOptions: {
+            includePaths: [].concat(
+                bourbon.includePaths,
+                neat.includePaths
+            )
+        }
     })
     .options({
         processCssUrls: false,
-    }).version();
+    })
+    .version();
